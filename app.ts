bcdd7ef1d78110c9d3e4164d0f46d7fe53e3097b
Jack Collins <BlueSkyFlyer2@gmail.com>
2016-02-09 20:01:02 -0700

    Home Page markup

--- app.ts
+++ app.ts
@@ -1,22 +1,28 @@
 import { bootstrap } from "angular2/platform/browser";
 import { Component } from "angular2/core";
-import { NgFor } from "angular2/common";
 
 @Component({
-    selector: 'hello-world',
+    selector: 'reddit',
     template: `
-    <ul>
-        <li *ngFor="#name of names">Hello {{ name }}!</li>
-    </ul>
+        <form class="ui large form segment">
+            <h3 class="ui header">Add a Link</h3>
+
+            <div class="field">
+                <label for="title">Title:</label>
+                <input name="title">
+            </div>
+            <div class="field">
+                <label for="link">Link:</label>
+                <input name="link">
+            </div>
+        </form>
     `
 })
 
-class HelloWorld {
-    names: Array<string>;
+class FlyfreeMeApp {
 
     constructor() {
-        this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
     }
 }
 
-bootstrap(HelloWorld);
+bootstrap(FlyfreeMeApp);
